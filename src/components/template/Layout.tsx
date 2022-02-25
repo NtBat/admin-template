ebf3b0747576c245f1803c367c00f2e392d8ea6d
NtBat <na.than.fjv@gmail.com>
2022-02-25 17:22:43 -0300

    initial structure

---
+++ src/components/template/Layout.tsx
@@ -0,0 +1,25 @@
+import Content from "./Content";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+interface LayoutProps {
+  title: string;
+  subtitle: string;
+  children?: any;
+}
+
+export default function Layout(props: LayoutProps) { 
+  return (
+    <div className={`
+      flex h-screen w-screen 
+    `}>
+        <Sidebar />
+      <div className={`flex flex-col w-full p-7 bg-gray-300`}>
+        <Header title={props.title} subtitle={props.subtitle} />
+        <Content>
+          {props.children}
+        </Content>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Layout from '../components/template/Layout'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Layout title="Home" subtitle="Developing...">
+      Content
+    </Layout>
   )
 }
