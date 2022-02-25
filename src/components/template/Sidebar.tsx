bd8c82954b83290cf3397e236bfdd7e13a5fa7fd
NtBat <na.than.fjv@gmail.com>
2022-02-25 18:02:40 -0300

    created MenuItem variation

--- src/components/template/Sidebar.tsx
+++ src/components/template/Sidebar.tsx
@@ -1,13 +1,27 @@
-import { HomeIcon, SettingsIcon, BellIcon } from "../icons";
+import { HomeIcon, SettingsIcon, BellIcon, LogoutIcon } from "../icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function Sidebar() {
   return (
-    <aside>
-      <ul>
+    <aside className={`flex flex-col`}>
+      <div className={`
+        w-20 h-20 bg-gradient-to-r from-indigo-500 to-purple-800
+        flex flex-col items-center justify-center
+      `}>
+        <Logo />
+      </div>
+      <ul className={`flex-grow`}>
         <MenuItem url="/" text="Home" icon={HomeIcon} />
         <MenuItem url="/settings" text="Settings" icon={SettingsIcon} />
         <MenuItem url="/notifications" text="Notifications" icon={BellIcon} />
+      </ul>
+      <ul>
+        <MenuItem 
+          text="Logout" icon={LogoutIcon} 
+          className={`text-red-600 hover:bg-red-500 hover:text-white`}
+          onClick={() => console.log('logout')} 
+        />
       </ul>
     </aside>
   )
