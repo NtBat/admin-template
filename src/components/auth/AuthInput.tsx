a0f813a2d82406058b49813cd4e2f4bfd560315a
NtBat <na.than.fjv@gmail.com>
2022-03-19 19:48:48 -0300

    created auth page css

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -10,15 +10,20 @@ interface AuthInputProps {
 export default function AuthInput(props: AuthInputProps) {
   return props.notRender ? null : (
     (
-      <div className={`flex flex-col`}>
-        <label>
+      <div className={`flex flex-col mt-4`}>
+        <label>{props.label}</label>
           <input 
             type={props.type ?? 'text'}        
             value={props.value} 
             onChange={e => props.changeValue?.(e.target.value)}  
-            required={props.required}     
+            required={props.required}
+            className={`
+              px-4 py-3 w-full rounded-lg bg-gray-200 mt-2
+              border focus:border-blue-500
+              focus:outline-none focus:bg-white
+            `}    
           />
-        </label>
+        
       </div>
     )
   )
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,14 +1,29 @@
 import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
 
+import {GoogleIcon} from '../components/icons';
+
 export default function Auth() {
   const [mode, setMode] = useState<'login' | 'signup'>('login');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  function submit() {
+    if(mode === 'login' ) {
+      console.log('login')
+    } else {
+      console.log('signup')
+    }
+  }
+
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className="flex flex-col h-screen items-center justify-center">
+    <div className="w-1/2">
+      <h1 className={`
+        text-xl font-bold mb-5
+      `}>
+        {mode === 'login' ? 'Login' : 'Signup'}
+      </h1>
       <AuthInput 
         label="Email"
         type="email"
@@ -23,6 +38,25 @@ export default function Auth() {
         changeValue={setPassword}
         required
       />
+
+      <button onClick={submit} className={`
+        w-full bg-indigo-500 hover:bg-indigo-400
+        text-white rounded-lg px-4 py-3 mt-6
+      `}>
+        {mode === 'login' ? 'Login' : 'Signup'}
+      </button>
+
+      <hr className="my-6 border-gray-300 w-full" />
+
+      <button onClick={submit} className={`
+        w-full bg-red-500 hover:bg-red-400
+        text-white rounded-lg px-4 py-3 
+        flex items center justify-center gap-1
+      `}>
+        {GoogleIcon}
+        Login with google
+      </button>
+    </div>
     </div>
   )
 }
