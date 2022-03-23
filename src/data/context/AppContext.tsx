9dfa75474d5eef380eda6043d1fad287b95dd920
NtBat <na.than.fjv@gmail.com>
2022-03-23 17:38:11 -0300

    add dark theme in localstorage

--- src/data/context/AppContext.tsx
+++ src/data/context/AppContext.tsx
@@ -1,21 +1,24 @@
-import { createContext, useState } from "react";
-
-type Theme = 'dark' | '';
-
+import { createContext, useEffect, useState } from "react";
 interface AppContextProps {
-  theme?: Theme;
+  theme?: string;
   changeTheme?: () => void;
 }
 
 const AppContext = createContext<AppContextProps>({})
 
 export function AppProvider(props) {
-
-  const [theme, setTheme] = useState<Theme>('dark');
+  const [theme, setTheme] = useState('dark');
 
   function changeTheme() {
-   setTheme(theme === '' ? 'dark' : '');
+    const newTheme = theme === '' ? 'dark' : ''
+   setTheme(newTheme);
+   localStorage.setItem('theme', newTheme);
   }
+
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    setTheme(theme)
+  }, [])
 
   return (
     <AppContext.Provider value={{
